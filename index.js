6c40b650196e7516ba5f320eb1106bc5176a9662
SemchaAI <76156008+SemchaAI@users.noreply.github.com>
2023-03-29 12:45:17 +0300

    +global js

---
+++ index.js
@@ -0,0 +1,7 @@
+const nav = document.getElementById("nav");
+const phone = document.getElementById("phone");
+
+function toggleClass() {
+  nav.classList.toggle("none"); // Переключаем класс hide
+  phone.classList.toggle("none");
+}
